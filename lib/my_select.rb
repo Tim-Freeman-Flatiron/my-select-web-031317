76bd0db5081932b4b359bbf165086e0519b14c43
Tim Freeman <d.timothy.freeman@gmail.com>
2017-02-08 14:57:28 -0500

    My Select -- Passed Tests

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+	if collection.empty? == false
+		i = 0
+		selection = []
+		while i < collection.size
+			if yield collection[i]
+				selection << collection[i]
+			end
+			i += 1
+		end
+	else
+		puts "This collection is empty."
+	end
+	selection
 end
